4bf8f24034206fd19b9355cfea2a3f3c61dd2f27
Senshi04 <125262209+Senshi04@users.noreply.github.com>
2023-06-05 14:12:05 +0200

    Update Chapeau.java

--- src/Chapeau.java
+++ src/Chapeau.java
@@ -5,69 +5,56 @@ import ardoise.PointPlan;
 import ardoise.Segment;
 
 public class Chapeau extends Forme {
-	private PointPlan p1;
-	private PointPlan p2;
-	private PointPlan p3;
+	private ArrayList<PointPlan> points;
 
 	public Chapeau() {
+		this.points = new ArrayList<PointPlan>(3);
 	}
 	
 
 	public Chapeau(String arg0,PointPlan p1, PointPlan p2, PointPlan p3) {
 		super(arg0);
 		// TODO Auto-generated constructor stub
-		this.p1 = p1;
-		this.p2 = p2;
-		this.p3 = p3;		
+		this.points = new ArrayList<PointPlan>(3);
+		this.points.add(p1);
+		this.points.add(p2);
+		this.points.add(p3);		
 	}
 	public Chapeau(Chapeau c) {
 		super(c.getNomForme());
-		this.p1 = c.p1;
-		this.p2 = c.p2;
-		this.p3 = c.p3;
+		this.points.add(c.getPoint(0));
+		this.points.add(c.getPoint(1));
+		this.points.add(c.getPoint(2));
 	}
 	
-	public PointPlan getP1() {
-		return this.p1;
+	public PointPlan getPoint(int index) {
+		return this.points.get(index);
 	}
 	
-	public PointPlan getP2() {
-		return this.p2;
+	public void setPoint(int index, PointPlan p) {
+		this.points.set(index,p);
 	}
 	
-	public PointPlan getP3() {
-		return this.p3;
-	}
-	
-	public void setP1(PointPlan p) {
-		this.p1 = p;
-	}
-	
-	public void setP2(PointPlan p) {
-		this.p2 = p;
-	}
-	
-	public void setP3(PointPlan p) {
-		this.p3 = p;
-	}
 	@Override
 	public void deplacer(int arg0, int arg1) {
 		// TODO Auto-generated method stub
-		this.p1.setAbscisse(arg0);
-		this.p1.setOrdonnee(arg1);
+		this.points.get(0).setAbscisse(arg0);
+		this.points.get(0).setOrdonnee(arg1);
 		
-		this.p2.setAbscisse(arg0);
-		this.p2.setOrdonnee(arg1);
+		this.points.get(1).setAbscisse(arg0);
+		this.points.get(1).setOrdonnee(arg1);
 		
-		this.p3.setAbscisse(arg0);
-		this.p3.setOrdonnee(arg1);
+		this.points.get(2).setAbscisse(arg0);
+		this.points.get(2).setOrdonnee(arg1);
+		
+		
 	}
 
 	@Override
 	public ArrayList<Segment> dessiner() {
-		ArrayList<Segment> s = new ArrayList<Segment>();
-		s.add(new Segment(p1,p2));
-		s.add(new Segment(p2,p3));
+		ArrayList<Segment> s = new ArrayList<Segment>(2);
+		s.add(new Segment(this.points.get(0),this.points.get(1)));
+		s.add(new Segment(this.points.get(1),this.points.get(2)));
 		return s;
 	}
 
@@ -77,3 +64,4 @@ public class Chapeau extends Forme {
 	}
 
 }
+
