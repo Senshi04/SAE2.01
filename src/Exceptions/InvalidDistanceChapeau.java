17f9abfb196c7872baa74ef05ddb4e32c26cb355
Senshi04 <125262209+Senshi04@users.noreply.github.com>
2023-06-12 10:10:05 +0200

    Add files via upload

---
+++ src/Exceptions/InvalidDistanceChapeau.java
@@ -0,0 +1,12 @@
+
+@SuppressWarnings("serial")
+public class InvalidDistanceChapeau extends Exception {
+	public InvalidDistanceChapeau(){
+		super();
+	}
+	
+	public String toString() {
+		return "La distance entre p1 et p3 est invalide. \nIl faut que la distance entre p1 et p2 soit la même entre p2 et p3 !";
+	}
+
+}
